23db6c7703813be9d6490d1fe4606a78446fdb64
Antonio Mata <hildeamp@gmail.com>
2021-02-06 01:11:54 -0600

    feat: add route specification

--- src/server.js
+++ src/server.js
@@ -1,7 +1,27 @@
 const fastify = require('fastify')({ logger: true });
 
-fastify.get('/', async (request, reply) => {
-	return { hello: 'world' };
+fastify.route({
+	method: 'GET',
+	url: '/',
+	schema: {
+		querystring: { name: { type: 'string' } },
+		response: {
+			200: {
+				type: 'object',
+				properties: {
+					hello: { type: 'string' },
+				},
+			},
+		},
+	},
+	preHandler: async (request, reply) => {
+		/* This could be awesomely useful for JWT and other validations */
+		console.log('request: ', request);
+		console.log('reply: ', reply);
+	},
+	handler: () => {
+		return { hello: 'world' };
+	},
 });
 
 const start = async () => {
